b47d2e96d3d0c9d46a97e676c189c76dbef57a3f
Magnus Krahl <magnuskrahl2@gmail.com>
2025-08-25 16:11:35 +0200

    extra funktionalitet til "tilfaeldigt-tal" opgaven

--- tilfaeldigt-tal/script.js
+++ tilfaeldigt-tal/script.js
@@ -1,15 +1,21 @@
 const randNumSelctor = document.querySelector("#randnum");
 
-randNumSelctor.innerHTML = "[tryk]";
+randNumSelctor.innerHTML = "[tryk her eller på mellemrum]";
+
+//lidt kode som gør at RNG funktionen også bliver kaldt når mellemrum bliver trykket. ved godt det er ekstra men har det sjovt tihi :)
+document.addEventListener("keydown", (event) => {
+  if (event.code === "Space") {
+    getRandNum();
+    event.preventDefault(); // stopper browserens "default" handling når mellemrum bliver trykker (typisk at scrolle nedad)
+  }
+});
 
 function getRandNum() {
-    let randNum = Math.floor(Math.random() * 100) + 1;
-    randNumSelctor.innerHTML = `[${randNum}]`; ///kantede paranteser [] er kun et æstetisk valg
-
+  let randNum = Math.floor(Math.random() * 100) + 1;
+  randNumSelctor.innerHTML = `[${randNum}]`; ///kantede paranteser [] er kun et æstetisk valg
 }
 
-
-randNumSelctor.addEventListener("mousedown", getRandNum)
+randNumSelctor.addEventListener("mousedown", getRandNum);
 
 ///debug:
 // randNumSelctor.innerHTML = "[randnum]";
