af565f0930359b48156c9264abe687bc96b9f56a
stinkjunk <magnuskrahl2@gmail.com>
2025-09-08 18:03:29 +0200

    themeswitcher

---
+++ theme-switcher/script.js
@@ -0,0 +1,27 @@
+const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+console.log("prefersDark: ", prefersDark);
+
+const themeSelector = document.querySelector("[data-theme]");
+const selecTheme = document.querySelector("#theme");
+const sysTheme = document.querySelector("#sysTheme");
+
+let currentTheme;
+if (prefersDark === true) {
+    currentTheme = "dark";
+    sysTheme.textContent = "Dark";
+} else {
+    currentTheme = "";
+    sysTheme.textContent = "Light";
+}
+
+selecTheme.addEventListener("change", () => {
+    currentTheme = selecTheme.value;
+    if (currentTheme === "system") {
+        currentTheme = prefersDark ? "dark" : "";
+    }
+    themeSelector.setAttribute("data-theme", currentTheme);
+});
+
+themeSelector.setAttribute("data-theme", currentTheme);
+
+
